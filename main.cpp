75cdec57c31b4017ad7007fc2bc15aac94e2847b
dikey <dikey.huang@chroma.com.tw>
2020-05-07 16:33:50 +0800

    test2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+//====================================
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
