eb83cee04f4fca11e2a9396c28acc0e59013b226
dikey <dikey.huang@chroma.com.tw>
2020-05-07 16:40:46 +0800

    dikey-test3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-//====================================
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -11,6 +11,6 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
-
+dikey-test3
     return app.exec();
 }
